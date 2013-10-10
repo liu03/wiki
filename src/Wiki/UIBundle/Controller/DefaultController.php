3bf39a60ed5e541c4ced54064e7e3c6bdcc40b9a
qiang <qiang@webdealauto.com>
2013-10-10 18:31:12 +0200

    +

--- src/Wiki/UIBundle/Controller/DefaultController.php
+++ src/Wiki/UIBundle/Controller/DefaultController.php
@@ -3,11 +3,30 @@
 namespace Wiki\UIBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Wiki\UIBundle\Entity\Category;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        if($request->getMethod()=='POST') {
+            $gender = $this->getDoctrine()
+                ->getRepository('WikiUIBundle:Gender')
+                ->find(1);
+            $article = new Category();
+            $article->setLabel('A Foo Bar');
+            $article->setLevel(3);
+            $article->setCreatedAt(new \DateTime());
+            $article->setDescription($request->request->get('des'));
+            $article->setStatus(1);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+        }
+
         return $this->render('WikiUIBundle:Default:index.html.twig', array('name' => 'shit'));
     }
 }
